dcefdc133313760d5217552c3c5864789e21b4ee
leg1tli <liamstair@outlook.com>
2025-12-13 13:12:13 +0100

    fixed reports.py file

--- expense_tracker/reports.py
+++ expense_tracker/reports.py
@@ -1,15 +1,13 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
-from .db import get_connection
+from db import get_connection
 
 def montly_summary_chart(year_month):
     conn = get_connection()
-    df = pd.read_sql_query("SELECT * FROM expenses",Conn) 
+    df = pd.read_sql_query("SELECT * FROM expenses", conn) 
     conn.close()
 
-    mdf = df[
-        fd['date'].str.startswith(year_month)
-        ]
+    mdf = df[df['date'].str.startswith(year_month)]
     g = mdf.groupby('category')['amount'].sum()
 
     g.plot(kind='bar', title=f"Expense summary for ({year_month})")
